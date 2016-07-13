f0b8c63f989ec0c84ccfb50413450ecc00cc9d41
hulyamacar1 <hulya.macaroglu@bil.omu.edu.tr>
2016-07-13 11:12:41 +0300

    YArdımcı gemler kuruldu

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orn1::Application.config.secret_key_base = '7ba267526aaa5150c16f402d3e196c9e2529bfa546f4fbe916d39b242d2038d1dc8324f6fcb2f120ffa7055bab112fa1ad4173a21f01e1779344b897aa520be9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Orn1::Application.config.session_store :cookie_store, key: '_orn1_session'
